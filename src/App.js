35c3da7b8057afe3b9e011f1f27ddbd27edab6dc
Pannu786 <thepannu786@gmail.com>
2022-06-05 00:14:43 +0530

    All dne

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import { Stack } from '@mui/material';
-import { Box } from '@mui/system';
 import Feed from './components/Feed';
 import Navbar from './components/Navbar';
 import Rightbar from './components/Rightbar';
 import Sidebar from './components/Sidebar';
 import Add from './components/Add';
+import { useState } from 'react';
+import { Box, createTheme, Stack, ThemeProvider } from '@mui/material';
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
   return (
-    <Box>
-      <Navbar />
-      <Stack direction='row' spacing={2} justifyContent='space-between'>
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-      </Stack>
-      <Add />
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={'background.default'} color={'text.primary'}>
+        <Navbar />
+        <Stack direction='row' spacing={2} justifyContent='space-between'>
+          <Sidebar setMode={setMode} mode={mode} />
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -16,7 +16,7 @@ import {
   Switch,
 } from '@mui/material';
 
-const Sidebar = () => {
+const Sidebar = ({ setMode, mode }) => {
   return (
     <Box flex={1} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
       <Box position='fixed'>
@@ -66,7 +66,9 @@ const Sidebar = () => {
               <ListItemIcon>
                 <ModeNight />
               </ListItemIcon>
-              <Switch />
+              <Switch
+                onChange={(e) => setMode(mode === 'light' ? 'dark' : 'light')}
+              />
             </ListItemButton>
           </ListItem>
         </List>
